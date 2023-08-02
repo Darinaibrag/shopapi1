1db5c813dd4ec3ca9a0043fc18511f2bb32433ed
Atai <zver.cm@gmail.com>
2023-08-02 18:54:46 +0600

    static fix

--- shopApi/settings.py
+++ shopApi/settings.py
@@ -175,3 +175,12 @@ CKEDITOR_CONFIGS = {
         'width': '100%',
     },
 }
+
+
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
